62f6cecae7461b39a564bfa6891f8911ed4c84db
JoseWRPereira <josewrpereira@tutanota.com>
2022-08-16 17:46:02 -0300

    Teste do módulo ESP01 com comando AT

--- main.c
+++ main.c
@@ -5,42 +5,25 @@
 #include "eusart.h"
 #include "error.h"
 
-extern unsigned char * eusart_rx_ptr0;
-extern unsigned char * eusart_rx_ptr1;
+#define VETOR_SIZE  16
+unsigned char vetor[VETOR_SIZE];
+const unsigned char msg[] = "AT\r\n";
 
-unsigned char vetor[16];
-const unsigned char msg[] = "CPU BT WiFi t9  ";
 void main( void )
 {
     lcd_init();
     eusart_init(115200);
 
-    for( char i=0; i<16; i++ )
+    for( char i=0; i<VETOR_SIZE; i++ )
         vetor[i] = 0;
-    for( char i=0; i<10; i++ )
-        vetor[i] = '.';
 
     lcd_print(0,0, msg );
-    lcd_print(1,0,"?");
-    lcd_put('$');
-
-    delay(3000);
-
-    eusart_read( vetor, 10 );
-    eusart_put('S');
-    eusart_put('E');
-    eusart_put('N');
-    eusart_put('A');
-    eusart_put('I');
+    eusart_read( vetor, VETOR_SIZE-1 );
     eusart_print(msg);
 
     while( 1 )
     {
-        lcd_num(0,0, (int)eusart_rx_ptr0, 5);
-        lcd_num(0,5, (int)eusart_rx_ptr1, 5);
-        lcd_num(0,10, (int)&vetor[0], 5);
         lcd_print(1,0,vetor);
-
 
         if( error_status() )
         {
