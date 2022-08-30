1911548c382cb4281aef19ac2892af1ca9d7d7f1
JoseWRPereira <josewrpereira@tutanota.com>
2022-08-30 15:30:48 -0300

    teste de conexão com ThingSpeak

--- interrupt.c
+++ interrupt.c
@@ -7,15 +7,15 @@ void __interrupt() isr(void)
 {
     if( PIR1bits.RCIF && PIE1bits.RCIE )
     {
-        // if( RCSTAbits.FERR )
-        // {
-        //     error_set(RECEIVE_FRAMING_ERROR);
-        // } 
-        // else if( RCSTAbits.OERR )
-        // {
-        //     error_set(RECEIVE_OVERRUN_ERROR);
-        // }
-        // else 
+        if( RCSTAbits.FERR )
+        {
+            error_set(RECEIVE_FRAMING_ERROR);
+        } 
+        else if( RCSTAbits.OERR )
+        {
+            error_set(RECEIVE_OVERRUN_ERROR);
+        }
+        else 
         {
             eusart_reading( RCREG );
         }
--- main.c
+++ main.c
@@ -6,93 +6,168 @@
 #include "error.h"
 #include "keyboard.h"
 
-#define RX_BUF_SIZE                     65
-unsigned char rx_buf[RX_BUF_SIZE];
-unsigned char rx_indice = 0;
-
+unsigned char estado = 0;
 const unsigned char * tx_ptr;
-unsigned char tx_indice = 0;
 
 void main( void )
 {
-    unsigned char tecla = 0;
-    unsigned char estado = 0;
-    
     lcd_init();
-    keyboard_init();
     eusart_init(115200);
+    keyboard_init();
 
     while( 1 )
     {
-        tecla = keyboard_scan();
-
         switch( estado )
         {
-            case 0:
-                    tx_indice = 0;
-                    rx_indice = 0;
-                    for( char i=0; i<RX_BUF_SIZE; i++ )
-                        rx_buf[i] = 0;
-                    ++estado;
-                    break;
-            case 1:
+            case 0: 
                     tx_ptr = "ATE0\r\n";
                     eusart_print(tx_ptr);
-                    ++estado;
+                    estado = 1;
                     break;
-
-            case 3:
-                    tx_ptr = "AT+CWMODE=2\r\n";
+            case 1: 
+                    delay(1000);
+                    estado = 2;
+                    break;
+            case 2: 
+                    tx_ptr = "AT+CWMODE=1\r\n";
                     eusart_print(tx_ptr);
-                    ++estado;
+                    estado = 3;
                     break;
-
-            case 5:
-                    tx_ptr = "AT+CWSAP=\"jwrp\",\"12345678\",1,3,4,0\r\n";
+            case 3: 
+                    delay(1000);
+                    estado = 4;
+                    break;
+            case 4: 
+                    tx_ptr = "AT+CWJAP=\"arduino\",\"12345678\"\r\n";
                     eusart_print(tx_ptr);
-                    ++estado;
+                    estado = 5;
                     break;
-
-            case 7:
-                    tx_ptr = "AT+CIPAP=\"192.168.4.80\"\r\n";
+            case 5: 
+                    delay(1000);
+                    estado = 6;
+                    break;
+            case 6: 
+                    tx_ptr = "AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",80\r\n";
                     eusart_print(tx_ptr);
-                    ++estado;
+                    estado = 7;
                     break;
-            case 9:
-                    tx_ptr = "AT+CIPMUX=1\r\n";
+            case 7: 
+                    delay(1000);
+                    estado = 8;
+                    break;
+            case 8: 
+                    tx_ptr = "AT+CIPSEND=84\r\n";
                     eusart_print(tx_ptr);
-                    ++estado;
+                    estado = 9;
                     break;
-            case 11:
-                    tx_ptr = "AT+CIPSERVER=1,333\r\n";
+            case 9: 
+                    delay(1000);
+                    estado = 10;
+                    break;
+            case 10: 
+                    tx_ptr = "GET https://api.thingspeak.com/update?api_key=6WFOK02JI7PJRJRA&field1=35&field2=40\r\n";
                     eusart_print(tx_ptr);
-                    ++estado;
+                    estado = 11;
                     break;
-            default:
-                    estado += (tecla=='<');
+            case 11: 
+                    delay(60000);
+                    estado = 6;
                     break;
         }
+        lcd_print(0,0, tx_ptr );
+        lcd_num(1,0, (int)estado, 2 );
 
-        if( tecla == '>' )
-        {
-            rx_indice += (rx_indice < (RX_BUF_SIZE-15));
-            // lcd_clr();
-        }
-       
-
-        if( estado % 2 )
-        {
-            rx_indice = 0;
-            for( char i=0; i<(RX_BUF_SIZE-1); i++ )
-                rx_buf[i] = ' ';
-            rx_buf[16] = 0;
-            eusart_read( rx_buf, RX_BUF_SIZE-1 );
-        }
-
-        lcd_print(0,0, tx_ptr+rx_indice );
-        lcd_print(1,0, rx_buf+rx_indice );
     }
 }
+
+
+// #define RX_BUF_SIZE                     65
+// unsigned char rx_buf[RX_BUF_SIZE];
+// unsigned char rx_indice = 0;
+
+// const unsigned char * tx_ptr;
+// unsigned char tx_indice = 0;
+
+// void main( void )
+// {
+//     unsigned char tecla = 0;
+//     unsigned char estado = 0;
+    
+//     lcd_init();
+//     keyboard_init();
+//     eusart_init(115200);
+
+//     while( 1 )
+//     {
+//         tecla = keyboard_scan();
+
+//         switch( estado )
+//         {
+//             case 0:
+//                     tx_indice = 0;
+//                     rx_indice = 0;
+//                     for( char i=0; i<RX_BUF_SIZE; i++ )
+//                         rx_buf[i] = 0;
+//                     ++estado;
+//                     break;
+//             case 1:
+//                     tx_ptr = "ATE0\r\n";
+//                     eusart_print(tx_ptr);
+//                     ++estado;
+//                     break;
+
+//             case 3:
+//                     tx_ptr = "AT+CWMODE=2\r\n";
+//                     eusart_print(tx_ptr);
+//                     ++estado;
+//                     break;
+
+//             case 5:
+//                     tx_ptr = "AT+CWSAP=\"jwrp\",\"12345678\",1,3,4,0\r\n";
+//                     eusart_print(tx_ptr);
+//                     ++estado;
+//                     break;
+
+//             case 7:
+//                     tx_ptr = "AT+CIPAP=\"192.168.4.80\"\r\n";
+//                     eusart_print(tx_ptr);
+//                     ++estado;
+//                     break;
+//             case 9:
+//                     tx_ptr = "AT+CIPMUX=1\r\n";
+//                     eusart_print(tx_ptr);
+//                     ++estado;
+//                     break;
+//             case 11:
+//                     tx_ptr = "AT+CIPSERVER=1,333\r\n";
+//                     eusart_print(tx_ptr);
+//                     ++estado;
+//                     break;
+//             default:
+//                     estado += (tecla=='<');
+//                     break;
+//         }
+
+//         if( tecla == '>' )
+//         {
+//             rx_indice += (rx_indice < (RX_BUF_SIZE-15));
+//             // lcd_clr();
+//         }
+       
+
+//         if( estado % 2 )
+//         {
+//             rx_indice = 0;
+//             for( char i=0; i<(RX_BUF_SIZE-1); i++ )
+//                 rx_buf[i] = ' ';
+//             rx_buf[16] = 0;
+//             eusart_read( rx_buf, RX_BUF_SIZE-1 );
+//         }
+
+//         lcd_print(0,0, tx_ptr+rx_indice );
+//         lcd_print(1,0, rx_buf+rx_indice );
+//     }
+// }
 
 
 
@@ -121,7 +196,12 @@ void main( void )
 
 
 
-
+// AT
+// AT+CWMODE=1
+// AT+CWLAP
+// AT+CWJAP="arduino","12345678"
 // AT+CIPSTART="TCP","api.thingspeak.com",80
-// AT+CIPSEND=74
-// GET https://api.thingspeak.com/update?api_key=6WFOK02JI7PJRJRA&field2=50
+// AT+CIPSEND=84
+// GET https://api.thingspeak.com/update?api_key=6WFOK02JI7PJRJRA&field1=35&field2=40
+
+
