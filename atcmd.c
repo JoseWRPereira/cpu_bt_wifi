768a1f62b1991857bd58b2b128bf0764d39baab3
JoseWRPereira <josewrpereira@tutanota.com>
2022-09-21 22:20:46 -0300

    atualização dos comandos AT

--- atcmd.c
+++ atcmd.c
@@ -1,55 +1,50 @@
 #include <xc.h>
 #include "atcmd.h"
 #include "eusart.h"
-#include "fsm.h"
 
-const char cmd_AT[] = "AT\r\n"; 
-
-void * atcmd_init( void * p )
+void fsm_atcmd_init( ATCMD * ptr )
 {
-    eusart_init( (unsigned long)(*(struct fsmT *)p).arg );
-
-    return( atcmd_send );
+    eusart_init( 115200 );
+    ptr->estado = 0;
 }
 
-void * atcmd_send( void * p )
+void fsm_atcmd_com( ATCMD * ptr, 
+                const char * str_send, 
+                const char * str_recv )
 {
-    eusart_print( cmd_AT );
-    return( atcmd_wait_ok );
+    ptr->estado = 1;
+    ptr->send = str_send;
+    ptr->recv = str_recv;
+    ptr->recv_size = size_of_str(ptr->recv);
 }
 
-void * atcmd_wait_ok( void * p )
+void fsm_atcmd( ATCMD * ptr )
 {
-    if( eusart_size_of_rx() >= size_of_str(cmd_AT) )
+    switch( ptr->estado )
     {
-        if( eusart_cmp( "OK" ) )
-        {
-            eusart_rx_pop();
-            return( atcmd_send );
-        }
+        case 1:
+                eusart_print( ptr->send );
+                ptr->estado = 10;
+                break;
+        case 10:
+                if( eusart_size_of_rx() >= ptr->recv_size )
+                {
+                    if( eusart_cmp( ptr->recv, ptr->recv_size ) )
+                    {
+                        ptr->estado = 20;
+                    }
+                    else
+                    {
+                        eusart_rx_pop();
+                    }
+                }
+                break;
+        case 20:
+                if( eusart_rx_pop() == '\n' )
+                    ptr->estado = ATCMD_EOT;
+                break;
+        case ATCMD_EOT:
+                eusart_rx_pop();
+                break;
     }
-    return( atcmd_wait_ok );
 }
-
-
-
-// void atcmd( struct atcmd_t * ptr )
-// {
-
-// }
-
-
-// void atcmd_finite_state_machine_scan( struct atcmd_t * ptr )
-// {
-//     switch( ptr->atcmd_state )
-//     {
-//         case ATCMD_WAITING:
-//                 break;
-//         case ATCMD_START:
-//                 eusart_read( ptr.rx, ATCMD_RX_SIZE );
-
-//                 eusart_print( ptr->ptr_send );
-
-//                 break;
-//     }
-// }
--- atcmd.h
+++ atcmd.h
@@ -1,31 +1,21 @@
 #ifndef ATCMD_H
 #define ATCMD_H
 
+#define ATCMD_EOT   100
 
 
-void * atcmd_init( void * p );
-void * atcmd_send( void * p );
-void * atcmd_wait_ok( void * p );
+typedef struct atcmd_t
+{
+    const char * send;
+    const char * recv;
+    unsigned char recv_size;
+    unsigned char estado;
+} ATCMD;
 
-
-
-
-// #define ATCMD_WAITING   0
-// #define ATCMD_START     1
-// #define ATCMD_END       0
-// #define ATCMD_ERROR     255
-
-// #define ATCMD_RX_SIZE   32
-
-// struct atcmd_t
-// {
-//     const char * ptr_send;
-//     const char * ptr_receive;
-//     char atcmd_state;
-//     char next_ok;
-//     char next_error;
-//     char rx[ATCMD_RX_SIZE];
-//     char i;
-// };
+void fsm_atcmd_init( ATCMD * ptr );
+void fsm_atcmd_com( ATCMD * ptr, 
+                const char * str_send, 
+                const char * str_recv );
+void fsm_atcmd( ATCMD * ptr );
 
 #endif
